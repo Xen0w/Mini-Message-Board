ba9ead9020ccbc90f18b4a42bb3c6fbe7c38b138
Samado Xen0w <benazouz.souheilabdessamed@gmail.com>
2024-12-28 00:28:00 +0100

    Mini

--- routes/new.js
+++ routes/new.js
@@ -3,9 +3,5 @@ const router = express.Router();
 router.get('/new', (req, res) => {
   res.render('form');
 });
-router.post('/new', (req, res) => {
-  const { author, messege } = req.body;
-  console.log(author + ' : ' + messege);
-  res.send('messege send seccecfuly');
-});
+
 export default router;
